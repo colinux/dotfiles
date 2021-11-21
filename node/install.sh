d8fd2d107a0ccc7a965bc98598b5ce36c3bc7a3f
Colin Darie <colin@darie.eu>
2021-11-21 15:23:30 +0100

    Node multiple versions support

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,19 @@
-#if test ! $(which spoof)
-#then
-#  if test $(which npm)
-#  then
-#    sudo npm install spoof -g
-#  fi
-#fi
+if test ! $(which yarn)
+then
+    npm install -g yarn
+fi
+
+if test ! $(which expo)
+then
+    npm install -g expo-cli eas-cli @expo/ngrok
+fi
+
+if test ! $(which lt)
+then
+    npm install -g localtunnel
+fi
+
+if test ! $(which react-devtools)
+then
+    npm install -g react-devtools @vue/devtools
+fi
